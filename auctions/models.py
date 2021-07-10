fd62b1547c7d4219ad53b214fe97b8bd362cefb5
Edward Boado <Eboado8862@conestogac.on.ca>
2021-07-09 21:14:43 -0400

    working on active listings

--- auctions/models.py
+++ auctions/models.py
@@ -5,19 +5,13 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Category(models.Model):
-    category =  models.CharField(max_length=100, help_text="Enter a category name with 100 characters or less.")
-
-    def __str__(self):
-        return f"{self.id}: {self.category}."
-
 class Listing(models.Model):
     poster_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster")
-    list_title = models.CharField(max_length=100, help_text="Enter a title with 100 characters or less.") 
-    category_id	= models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listing_category")
-    description	= models.CharField(max_length=500, help_text="Enter a description with 500 characters or less.")
+    list_title = models.CharField(max_length=100, help_text="Enter a title using 100 characters or less.")     
+    description	= models.CharField(max_length=500, help_text="Enter a description using 500 characters or less.")    
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)	
-    highest_bid = models.DecimalField(max_digits=10, decimal_places=2)	
+    category_id	= models.CharField(max_length=100)
+    image_url = models.CharField(max_length=300, help_text="Enter the URL of an image, using 300 characters or less.") 
     
     def __str__(self):
         return f"{self.list_title}, posted by {self.poster_id}."
@@ -33,10 +27,10 @@ class Bid(models.Model):
 class Comment(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_comment")
-    comment = models.CharField(max_length=500, help_text="Enter a comment with 500 characters or less.")
+    comment = models.CharField(max_length=500, help_text="Enter a comment using 500 characters or less.")
 
     def __str__(self):
-        return f"{self.user_id} commented: \"{self.comment}\" on {self.listing_id}."
+        return f"{self.user_id} commented: \"{self.comment}\" on {self.listing_id}."    
 
 class Watchlist(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watcher")     
--- auctions/views.py
+++ auctions/views.py
@@ -3,12 +3,23 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
+from .models import *
 
-from .models import User
+
+# form for creating a new listing
+class NewListingForm(forms.Form):
+    list_title = forms.CharField(widget=forms.TextInput(attrs={'size':100}), label = "Listing Title", max_length=100)
+    description = forms.CharField(widget=forms.TextInput(attrs={'size':100}), label = "Description", max_length=500)
+    starting_bid = forms.DecimalField(widget=forms.TextInput(attrs={'size':100}), label = "Starting Bid", max_digits=10, decimal_places=2)
+    category_id = forms.CharField(widget=forms.TextInput(attrs={'size':100}), label="Category (optional)", max_length=100, required=False)
+    image_url = forms.CharField(widget=forms.TextInput(attrs={'size':100}), label="Image URL (optional)", max_length=300, required=False)
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", {
+        "listings": Listing.objects.all()
+    })
 
 
 def login_view(request):
@@ -61,3 +72,28 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_new(request):
+    if request.method == "POST":
+        # Validate submitted listing
+        new_input = NewListingForm(request.POST)
+        if new_input.is_valid():    
+            # Save new listing
+            new_listing = Listing(
+                poster_id = request.user,
+                list_title = new_input.cleaned_data["list_title"],
+                description = new_input.cleaned_data["description"],
+                starting_bid = new_input.cleaned_data["starting_bid"],
+                category_id = new_input.cleaned_data["category_id"],
+                image_url = new_input.cleaned_data["image_url"],
+                )
+            new_listing.save()
+
+        # Display the index    
+        return HttpResponseRedirect(reverse("index"))
+
+    else:
+        return render(request, "auctions/create.html", {
+            "form": NewListingForm()
+        })
