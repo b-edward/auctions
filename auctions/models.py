3dece8faa468f22bcca2c99bfc9038645783402b
Edward Boado <Eboado8862@conestogac.on.ca>
2021-07-13 21:44:01 -0400

    working on bidding on listings

--- auctions/models.py
+++ auctions/models.py
@@ -22,7 +22,7 @@ class Bid(models.Model):
     bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.bidder_id} has bid ${self.bid_amount} on {self.listing_id}."
+        return f"${self.bid_amount}"
 
 class Comment(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
--- auctions/views.py
+++ auctions/views.py
@@ -6,10 +6,11 @@ from django.shortcuts import render
 from django.urls import reverse
 from django import forms
 from .models import *
+from . import util
 from django.contrib.auth.decorators import login_required
 
 
-# form for creating a new listing
+# Form for creating a new listing
 class NewListingForm(forms.Form):
     list_title = forms.CharField(widget=forms.TextInput(attrs={'size':100}), label = "Listing Title", max_length=100)
     description = forms.CharField(widget=forms.TextInput(attrs={'size':100}), label = "Description", max_length=1000)
@@ -17,13 +18,21 @@ class NewListingForm(forms.Form):
     category_id = forms.CharField(widget=forms.TextInput(attrs={'size':100}), label="Category (optional)", max_length=100, required=False)
     image_url = forms.CharField(widget=forms.TextInput(attrs={'size':100}), label="Image URL (optional)", max_length=300, required=False)
 
+# Form for bidding on an auction
+class NewBidForm(forms.Form):
+    listing_id = forms.CharField(widget=forms.TextInput(attrs={'size':10}), label = "Listing Title", max_length=1000)
+    bidder_id = forms.CharField(widget=forms.TextInput(attrs={'size':10}), label = "Bidder Name", max_length=1000) 
+    bid_amount = forms.DecimalField(widget=forms.TextInput(attrs={'size':100}), label = "Starting Bid", max_digits=10, decimal_places=2)
 
+
+# Main listing page
 def index(request):
     return render(request, "auctions/index.html", {
         "listings": Listing.objects.all()
     })
 
 
+# Login to existing account
 def login_view(request):
     if request.method == "POST":
 
@@ -44,11 +53,13 @@ def login_view(request):
         return render(request, "auctions/login.html")
 
 
+# Log out of account session
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
 
+# Create a new user account
 def register(request):
     if request.method == "POST":
         username = request.POST["username"]
@@ -75,6 +86,8 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
+
+# Create a new auction listing
 def create_new(request):
     if request.method == "POST":
         # Validate submitted listing
@@ -98,6 +111,7 @@ def create_new(request):
         return render(request, "auctions/create.html", {
             "form": NewListingForm()
         })
+
 
 # Call up the listing for this title
 def listing(request, title):
@@ -126,11 +140,26 @@ def listing(request, title):
     # Do nothing if listing doesn't exist
     else:    
         pass    
-        
-    # Send listing and title status
+
+
+    # Get the bids for this title
+    # bids = 
+    #
+    # Determine the highest bid
+    # highest_bid = util.high_bid(   )
+    #
+    # Create a new bid form
+    # new_bid = 
+    #
+
+    # Send listing, title status, bid form and highest bid
     return render(request, "auctions/listing.html", {
-        "listing": listing, "in_list": in_list
+        "listing": listing, 
+        "in_list": in_list,  
+        #"highest_bid": highest_bid
+        #"form": new_bid
     })
+
 
 # if user logged in, try to add the listing to their watchlist
 @login_required
@@ -141,7 +170,7 @@ def watchlist(request, title):
     
     # Check if the user has this title in the watchlist
     if users_list.filter(listing_id=listing.id):
-        message = "That listing is already in your watchlist"    # Let user know its already there
+        message = "Listing is already in your watchlist"    # Let user know its already there
         in_list = "True"
 
         # Send back to listing page with message
@@ -163,7 +192,7 @@ def watchlist(request, title):
     })
 
 
-# if user logged in, try to remove the listing from their watchlist
+# If user logged in, try to remove the listing from their watchlist
 @login_required
 def remove_watch(request, title):     
     listing = Listing.objects.get(list_title=title)             # Get the listing
@@ -184,9 +213,24 @@ def remove_watch(request, title):
         "users_list": users_list, "message": message
     })
 
-'''
-    # Send back to listing page with message
-    return render(request, "auctions/listing.html", {
-    "listing": listing, "in_list": in_list, 
+
+# Bid on an item
+@login_required
+def remove_watch(request, title):     
+    listing = Listing.objects.get(list_title=title)             # Get the listing
+    logged_user = request.user                                  # Get the user's id
+    to_remove = Watchlist.objects.filter(user_id=logged_user, listing_id=listing)  # Get the listing from users watchlist
+    
+    # Remove listing from the watchlist
+    to_remove.delete()  
+
+    # Advise listing removed
+    message = "The listing has been removed from your watchlist"     
+    in_list = "False"   
+    
+    users_list = Watchlist.objects.filter(user_id=logged_user)  # Get the users updated watchlist
+
+    # Send the users watchlist to the template
+    return render(request, "auctions/watchlist.html", {
+        "users_list": users_list, "message": message
     })
-'''
